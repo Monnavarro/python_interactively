6d058b9202a342a298f9a7492c4832283c898f70
Monnavarro <montserrat.nvro.lpz@gmail.com>
2022-10-22 11:53:51 +0200

    new: learnig tuples, lists and dictionaries

---
+++ list_tuples_dict/exem_tuples.py
@@ -0,0 +1,24 @@
+# -- coding: utf-8 --
+"""
+
+Created on: 22/10/22
+@author: Montse Navarro <montserrat.nvro.lpz@gmail.com>
+Licence,
+"""
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+another_tuple = tuple()
+
+abc = tuple([1, 2, 3])
+abc
+
+# Slicing
+my_tuple_slicing = my_tuple[:3]
+
+# cast de una tuple
+
+abc = tuple([1, 2, 3])
+abc
+abc_list = list(abc)
+abc_list
